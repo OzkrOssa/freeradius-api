fae260765eaf6bfec7f33b4695a1ccd3493aefd3
Oscar Ossa <oskrlex17@gmail.com>
2024-11-18 17:02:36 -0500

    feat: add not updatedcustom error

--- internal/core/domain/error_domain.go
+++ internal/core/domain/error_domain.go
@@ -3,7 +3,8 @@ package domain
 import "errors"
 
 var (
-	DataNotFoundError = errors.New("data not found")
-	InternalError     = errors.New("internal server error")
-	ConflictDataError = errors.New("data conflicts with existing data")
+	DataNotFoundError  = errors.New("data not found")
+	InternalError      = errors.New("internal server error")
+	ConflictDataError  = errors.New("data conflicts with existing data")
+	NoUpdatedDataError = errors.New("no data to update")
 )
