1c586d886c3f7bd69d3c3c5189a3036e470ffb1d
Oscar Ossa <oskrlex17@gmail.com>
2024-11-19 14:30:14 -0500

    feat: add additional errors for tokens and credentials

--- internal/core/domain/error_domain.go
+++ internal/core/domain/error_domain.go
@@ -3,8 +3,13 @@ package domain
 import "errors"
 
 var (
-	DataNotFoundError  = errors.New("data not found")
-	InternalError      = errors.New("internal server error")
-	ConflictDataError  = errors.New("data conflicts with existing data")
-	NoUpdatedDataError = errors.New("no data to update")
+	DataNotFoundError       = errors.New("data not found")
+	InternalError           = errors.New("internal server error")
+	ConflictDataError       = errors.New("data conflicts with existing data")
+	NoUpdatedDataError      = errors.New("no data to update")
+	TokenDurationError      = errors.New("invalid token duration format")
+	TokenCreationError      = errors.New("error creating token")
+	ExpiredTokenError       = errors.New("access token has expired")
+	InvalidTokenError       = errors.New("access token is invalid")
+	InvalidCredentialsError = errors.New("invalid email or password")
 )
