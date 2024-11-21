9aac82a2f86679f9a11b1e8154d91f3e031fd638
Oscar Ossa <oskrlex17@gmail.com>
2024-11-21 07:39:05 -0500

    feat: added response structures and utility functions to manage API responses

---
+++ internal/adapter/handler/response.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"github.com/OzkrOssa/freeradius-api/internal/core/domain"
+	"time"
+)
+
+type response struct {
+	Success bool   `json:"success" example:"true"`
+	Message string `json:"message" example:"Success"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func newResponse(success bool, message string, data any) response {
+	return response{
+		Success: success,
+		Message: message,
+		Data:    data,
+	}
+}
+
+type meta struct {
+	Total uint64 `json:"total" example:"100"`
+	Limit uint64 `json:"limit" example:"10"`
+	Skip  uint64 `json:"skip" example:"0"`
+}
+
+func newMeta(total, limit, skip uint64) meta {
+	return meta{
+		Total: total,
+		Limit: limit,
+		Skip:  skip,
+	}
+}
+
+type authResponse struct {
+	AccessToken string `json:"token" example:"v2.local.Gdh5kiOTyyaQ3_bNykYDeYHO21Jg2..."`
+}
+
+func newAuthResponse(accessToken string) authResponse {
+	return authResponse{
+		AccessToken: accessToken,
+	}
+}
+
+type userResponse struct {
+	ID        uint64    `json:"id" example:"1"`
+	Name      string    `json:"name" example:"John Doe"`
+	Email     string    `json:"email" example:"test@example.com"`
+	CreatedAt time.Time `json:"created_at" example:"1970-01-01T00:00:00Z"`
+	UpdatedAt time.Time `json:"updated_at" example:"1970-01-01T00:00:00"`
+}
+
+func newUserResponse(user *domain.User) userResponse {
+	return userResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
